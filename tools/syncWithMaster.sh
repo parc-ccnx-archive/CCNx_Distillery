0e17493db7cbb226f6c8974db2a3e55d55d871d7
Alan Walendowski <walendo@parc.com>
2016-02-03 18:16:05 -0800

    Check for existence of parc_upstream before attempting to sync with it

--- tools/syncWithMaster.sh
+++ tools/syncWithMaster.sh
@@ -1,4 +1,44 @@
 #!/bin/sh
+#
+# Copyright (c) 2016, Xerox Corporation (Xerox)and Palo Alto Research Center (PARC)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Patent rights are not granted under this agreement. Patent rights are
+#       available under FRAND terms.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL XEROX or PARC BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# @author Alan Walendowski, Palo Alto Research Center (PARC)
+# @copyright 2016, Xerox Corporation (Xerox)and Palo Alto Research Center (PARC).  All rights reserved.
+
+
+# This script is intended to sync origin/master with parc_upstream/master.
+# It will fetch the changes in parc_upstream, and merge them into to
+# origin/master. 
+# If you were on a different branch than master, you should still be on
+# that branch when the script exits.
+
+git remote show parc_upstream > /dev/null
+if [ $? -ne 0 ]; then
+    exit $?
+fi
 
 BRANCH=`git symbolic-ref --short HEAD`
 echo "Currently on " $BRANCH
