d9bec358bbf65a66331c5339a21191e02207a8ee
Nacho (Ignacio) Solis <isolis@igso.net>
2016-02-05 17:07:55 -0800

    Check for upstream

--- tools/getStatus.sh
+++ tools/getStatus.sh
@@ -8,19 +8,20 @@ cd ${GIT_DIRECTORY}
 CURRENT_SHA=`git branch -av | grep "\*" | awk '{print $3}'`
 CURRENT_BRANCH=`git branch -av | grep "\*" | awk '{print $2}'`
 UPSTREAM_SHA=`git branch -av | grep "remotes/parc_upstream/master" | awk '{print $2}'`
-PARC_MASTER_IN_BRANCH=`git branch -v --contains $UPSTREAM_SHA | grep "\*" | awk '{print $3}'`
+
+if [ x${UPSTREAM_SHA} = x ]; then
+  # We don't have an upstream...
+  UPSTREAM_SHA="NO_UPSTREAM"
+else
+  PARC_MASTER_IN_BRANCH=`git branch -v --contains $UPSTREAM_SHA | grep "\*" | awk '{print $3}'`
+fi
+
 
 echo '===================================================================='
 
-#echo CURRENT_SHA ${CURRENT_SHA}
-#echo CURRENT_BRANCH ${CURRENT_BRANCH}
-#echo UPSTREAM_SHA ${UPSTREAM_SHA}
-#echo PARC_MASTER_IN_BRANCH ${PARC_MASTER_IN_BRANCH}
-#echo "git branch --contains $UPSTREAM_SHA | grep \"\*\" | awk '{print $3}'"
-
 if [ "x${PARC_MASTER_IN_BRANCH}" != "x" ]; then
   # This branch is master OR ahead of master
-  if [ ${CURRENT_SHA} = ${UPSTREAM_SHA} ]; then
+  if [ x${CURRENT_SHA} = x${UPSTREAM_SHA} ]; then
     echo "PARC    ${GIT_REPO} ${CURRENT_BRANCH}"
   else
     echo "PARC++  ${GIT_REPO} ${CURRENT_BRANCH} ${CURRENT_SHA}::${UPSTREAM_SHA}"
