a6d6e48c6358e2c6469095f7bb1b73a938478b52
Christopher Wood <christopherwood07@gmail.com>
2016-03-24 09:16:57 -0700

    Add the module config file for the ccnxFileRepo application.

---
+++ config/modules/603-ccnxFileRepo.mk
@@ -0,0 +1,4 @@
+ccnxFileRepo_GIT_REPOSITORY=git@githubenterprise.parc.com:cwood/ccnxFileRepo.git
+ccnxFileRepo_GIT_UPSTREAM_REPOSITORY=git@githubenterprise.parc.com:cwood/ccnxFileRepo.git
+
+$(eval $(call addCMakeModule,ccnxFileRepo))
