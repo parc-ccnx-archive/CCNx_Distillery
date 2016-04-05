08d6f4c7fbb38efeff7eed9a5d5a65a6d077c679
Christopher Wood <christopherwood07@gmail.com>
2016-04-05 09:20:36 -0700

    Updated config module.

--- config/modules/603-ccnxFileRepo.mk
+++ config/modules/603-ccnxFileRepo.mk
@@ -1,4 +1,1 @@
-ccnxFileRepo_GIT_REPOSITORY=git@githubenterprise.parc.com:cwood/ccnxFileRepo.git
-ccnxFileRepo_GIT_UPSTREAM_REPOSITORY=git@githubenterprise.parc.com:cwood/ccnxFileRepo.git
-
 $(eval $(call addCMakeModule,ccnxFileRepo))
